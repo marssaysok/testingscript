f5578b26e6edb702cb027ee71258f7bca90a57e4
mars <140008207+marssaysok@users.noreply.github.com>
2024-11-09 20:08:59 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,8 +8,9 @@ echo -e "\033[1;35mcosmos.lol\033[0m"
 
 # Ask for the key
 while true; do
+  # Prompt user for input, ensuring it waits for input on the next line
   echo -n "> Enter your Key: "
-  read key
+  read -r key
   echo "> Validating Key..."
 
   # Replace "ownerkey123" with the actual valid key you want to use
