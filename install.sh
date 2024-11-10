5d8417f74645e71dba8730e048eba710f03cd445
mars <140008207+marssaysok@users.noreply.github.com>
2024-11-09 20:08:06 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,24 @@
-testing
+#!/bin/bash
+
+# Clear the terminal
+clear
+
+# Display "cosmos.lol" in purple
+echo -e "\033[1;35mcosmos.lol\033[0m"
+
+# Ask for the key
+while true; do
+  echo -n "> Enter your Key: "
+  read key
+  echo "> Validating Key..."
+
+  # Replace "ownerkey123" with the actual valid key you want to use
+  if [ "$key" == "ownerkey123" ]; then
+    echo "> Key Validated"
+    # Run the script you want to execute
+    python3 -c 'print("Hello")'
+    break
+  else
+    echo "> Key not valid"
+  fi
+done
